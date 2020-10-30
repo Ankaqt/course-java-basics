2126de1438b335609df06d9f48b1b6d98d318bc0
Admin <khramkova777@gmail.com>
2020-10-31 00:22:26 +0300

    add StudioTest

--- src/test/java/com/rakovets/course/javabasics/practice/jcf/list/StudioTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/jcf/list/StudioTest.java
@@ -1,4 +1,4 @@
-package com.rakovets.course.javabasics.practice.jcf.collection;
+package com.rakovets.course.javabasics.practice.jcf.list;
 
 import com.rakovets.course.javabasics.practice.jcf.Actor;
 import com.rakovets.course.javabasics.practice.jcf.Studio;
